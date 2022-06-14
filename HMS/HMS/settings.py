31f253179b322271dda7395043b3823842b9c33a
Ngô Nhật Tân <55774238+tanngo2510@users.noreply.github.com>
2022-06-14 22:15:54 +0700

    Update settings.py

--- HMS/HMS/settings.py
+++ HMS/HMS/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-# import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,7 +131,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR, "static"]
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
 
 # mail
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
